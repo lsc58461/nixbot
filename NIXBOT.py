182e61af9c2a2fd16113219c16cc14092f04f1d4
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-09-11 13:47:22 +0900

    Update NIXBOT.py

--- NIXBOT.py
+++ NIXBOT.py
@@ -72,11 +72,12 @@ async def Post_PatchNote():
             print(f"{now})  채널 이름:{channel}\n{now})  채널 ID:{Channel_ID_PatchNote}")
             print(f"{now})  채널 이름:{channel2}\n{now})  채널 ID:{Channel_ID_PatchNote2}")
             URL = await requests.get("https://kr.leagueoflegends.com/ko-kr/news/tags/patch-notes")
+            URL.encoding = 'UTF-8-SIG'
             URL = URL.text
 
             #패치노트 주소
             soup = BeautifulSoup(URL, 'html.parser')
-            PatchNote_URL_find = soup.find('a', {'class': 'style__Wrapper-i44rc3-0 style__ResponsiveWrapper-i44rc3-13 gkCnQM isVisible'})
+            PatchNote_URL_find = soup.find('a', {'class': 'style__Wrapper-sc-1h41bzo-0'})
             PatchNote_URL = "https://kr.leagueoflegends.com" + PatchNote_URL_find.get('href')
             print(f"{now})  패치노트 주소\n{now})    -{PatchNote_URL}")
 
@@ -104,6 +105,7 @@ async def Post_PatchNote():
             print(f"{now})  패치노트 저장 제목:{title2}")
 
             PatchNote_URL2 = await requests.get(PatchNote_URL)
+            PatchNote_URL2.encoding = 'UTF-8-SIG'
             soup = BeautifulSoup(PatchNote_URL2.text, "html.parser")
             PatchNote_Text = soup.find('blockquote', {'class': 'blockquote context'})
             PatchNote_Text = re.sub('<.+?>', '', str(PatchNote_Text), 0).strip()
@@ -119,7 +121,7 @@ async def Post_PatchNote():
             )
             MyEmbed.add_field(
                 name = "\n\u200b",
-                value =  "`" + PatchNote_Text[0:266] + "· · ·" + "`",
+                value =  '```css\n{} · · ·\n```'.format(PatchNote_Text[0:266]),
                 inline = True
             )
             MyEmbed.set_author(
@@ -129,6 +131,7 @@ async def Post_PatchNote():
             MyEmbed.set_image(
                 url = PatchNote_Image_URL
             )
+            
 
             await channel.send(embed=MyEmbed)
             await channel2.send(embed=MyEmbed)
@@ -139,121 +142,12 @@ async def Post_PatchNote():
         a = 1
         print(f"{now})  Post_PatchNote 에러 발생\n{now})    -{ex}")
 
-async def Post_Issues():
-    try:
-        global b
-        if b == 1:        
-            now = datetime.datetime.now()
-            print(f"{now})  전송 시작")
-            channel = client.get_channel(Channel_ID_Issues)
-            channel2 = client.get_channel(Channel_ID_Issues2)
-            print(f"{now})  채널 이름:{channel}\n{now})  채널 ID:{Channel_ID_Issues}")
-            print(f"{now})  채널 이름:{channel2}\n{now})  채널 ID:{Channel_ID_Issues2}")
-            Read_json = requestss.get("https://lol.secure.dyn.riotcdn.net/channels/public/x/status/kr1.json").json()
-            #FileName_json = "Issues.json"
-            #Read_json = json.loads(open(FileName_json, encoding='UTF-8-SIG').read())
-            for titles in Read_json['maintenances']:
-                for title in titles['titles'][1]['content'].split('\n'):
-                    print(title)
-            for maintenances in Read_json['maintenances']:
-                for updates in maintenances['updates']:
-                    for issues in updates['translations'][1]['content'].split('\n'):
-                        Issues = issues
-        
-            MyEmbed = discord.Embed(
-                title = "리그 오브 레전드 서버 상태",
-                url = "https://status.riotgames.com/lol?region=kr1&locale=ko_KR",
-                color = 0xFF5A5A
-            )
-            MyEmbed.set_thumbnail(
-                url = "https://cdn.discordapp.com/attachments/811123288352358441/831572153542639666/league_of_legends_sm.png"
-            )
-            MyEmbed.add_field(
-                name = title,
-                value =  Issues,
-                inline = True
-            )
-            MyEmbed.set_author(
-                name = "League of Legends",
-                icon_url = "https://cdn.discordapp.com/attachments/811123288352358441/811695474952110110/NIX.png"
-            )
-            await channel.send(embed=MyEmbed)
-            await channel2.send(embed=MyEmbed)
-            print(f"{now})  이슈 전송 성공")
-            b = 0
-            print(f"{now})  a == {b}")
-    except Exception as ex:
-        b = 1
-        print(f"{now})  Post_Issues 에러 발생\n{now})    -{ex}")
-
-async def Post_Issues_Empty():
-    try:
-        global c
-        global cc
-        if c == 1:
-            now = datetime.datetime.now()
-            mem = request.urlopen("http://fxserver.dothome.co.kr/DATA/Data_Issues_Empty.ini").read()
-            with open(Data_Issues_Empty_File, mode="wb") as f:
-                f.write(mem)
-
-            
-
-            config.read(Data_Issues_Empty_File, encoding='UTF-8-SIG') 
-            config.sections()
-            cc = int(config['Data']['Issues'])
-            
-            if cc == 0:
-                print(f"{now})  이슈 변경감지\n{now})  이슈:특이 사항 또는 문제 없음")
-                print(f"{now})  전송 시작")
-                channel = client.get_channel(Channel_ID_Issues)
-                channel2 = client.get_channel(Channel_ID_Issues2)
-                print(f"{now})  채널 이름:{channel}\n{now})  채널 ID:{Channel_ID_Issues}")
-                print(f"{now})  채널 이름:{channel2}\n{now})  채널 ID:{Channel_ID_Issues2}")
-        
-                MyEmbed = discord.Embed(
-                    title = "리그 오브 레전드 서버 상태",
-                    url = "https://status.riotgames.com/lol?region=kr1&locale=ko_KR",
-                    color = 0x38f2ff
-                )
-                MyEmbed.set_thumbnail(
-                    url = "https://cdn.discordapp.com/attachments/811123288352358441/831572153542639666/league_of_legends_sm.png"
-                )
-                MyEmbed.add_field(
-                    name = "\n\u200b",
-                    value =  "특이 사항 또는 문제 없음",
-                    inline = True
-                )
-                MyEmbed.set_author(
-                    name = "League of Legends",
-                    icon_url = "https://cdn.discordapp.com/attachments/811123288352358441/811695474952110110/NIX.png"
-                )
-                
-                await channel.send(embed=MyEmbed)
-                await channel2.send(embed=MyEmbed)
-                print(f"{now})  이슈 전송 성공")
-                c = 0
-                config['Data'] = {}
-                config['Data']['Issues'] = '1'
-                with open(Data_Issues_Empty_File, 'w', encoding='UTF-8-SIG') as configfile:
-                    config.write(configfile)
-                ftp = FTP('fxserver.dothome.co.kr')
-                ftp.login(os.environ["Server_ID"], os.environ["Server_PW"])
-                ftp.cwd('html/DATA')  # 업로드할 FTP 폴더로 이동
-                myfile = open(Data_Issues_Empty_File,'rb')  # 로컬 파일 열기
-                ftp.storbinary('STOR ' +Data_Issues_Empty_File, myfile )  # 파일을 FTP로 업로드
-                print(f"{now})  FTP {Data_Issues_Empty_File} 업로드 완료")
-                myfile.close()  # 파일 닫기
-                ftp.quit()
-                print(f"{now})  cc = 1 반환 완료")
-    except Exception as ex:
-        c = 1
-        print(f"{now})  Post_Issues_Empty 에러 발생\n{now})    -{ex}")
-
-@tasks.loop(seconds=10)
+@tasks.loop(seconds=5)
 async def Title_Detected():
     try:
         global a
         URL = await requests.get("https://kr.leagueoflegends.com/ko-kr/news/tags/patch-notes")
+        URL.encoding = 'UTF-8-SIG'
         now = datetime.datetime.now()
         soup = BeautifulSoup(URL.text, "html.parser")
         for a in soup.find('h2'):
@@ -293,91 +187,6 @@ async def Title_Detected():
             print(f"{now})  a = {a} 반환 완료")
     except Exception as ex:
         print(f"{now})  Title_Detected 에러 발생\n{now})    -{ex}")
-        
-async def Issues_Detected():
-    try:
-        global b
-        global c
-        File_Save_Issues = Data_Issues_File
-        now = datetime.datetime.now()
-        Read_json = requestss.get("https://lol.secure.dyn.riotcdn.net/channels/public/x/status/kr1.json").json()
-        #print(Read_json)
-        #FileName_json = "empty.json"
-        #Read_json = json.loads(open(FileName_json, encoding='UTF-8-SIG').read())
-
-        mem = request.urlopen("http://fxserver.dothome.co.kr/DATA/Data_Issues.ini").read()
-        with open(File_Save_Issues, mode="wb") as f:
-            f.write(mem)
-
-        config.read(Data_Issues_File, encoding='UTF-8-SIG') 
-        config.sections()
-        Issues2 = config['Data']['Issues']
-
-        if Read_json['maintenances'] == []:
-            issues = "Empty"
-            print(f"{now})  이슈 감지 중:특이 사항 또는 문제 없음")
-            
-        for maintenances in Read_json['maintenances']:
-            for locale in maintenances['titles'][1]['locale'].split('\n'):
-                if locale == "ko_KR":
-                    for maintenances in Read_json['maintenances']:
-                        for updates in maintenances['updates']:
-                            for issues in updates['translations'][1]['content'].split('\n'):
-                                print(f"{now})  이슈 감지 중:{issues}")
-
-        if Issues2 != issues:
-            print(f"{now})  이슈 변경감지\n{now})  이슈:{issues}")
-        
-            config['Data'] = {}
-            config['Data']['Issues'] = issues
-            with open(Data_Issues_File, 'w', encoding='UTF-8-SIG') as configfile:
-                config.write(configfile)
-            
-            ftp = FTP('fxserver.dothome.co.kr')
-            ftp.login(os.environ["Server_ID"], os.environ["Server_PW"])
-            ftp.cwd('html/DATA')  # 업로드할 FTP 폴더로 이동
-            myfile = open(FileName_Issues,'rb')  # 로컬 파일 열기
-            ftp.storbinary('STOR ' +FileName_Issues, myfile )  # 파일을 FTP로 업로드
-            print(f"{now})  FTP 업로드 완료")
-            myfile.close()  # 파일 닫기
-            ftp.quit()
-            b = 1
-            print(f"{now})  b = {b} 반환 완료")
-            
-        elif issues == "Empty":
-            config['Data'] = {}
-            config['Data']['Issues'] = "0"
-            with open(Data_Issues_Empty_File, 'w', encoding='UTF-8-SIG') as configfile:
-                config.write(configfile)
-            ftp = FTP('fxserver.dothome.co.kr')
-            ftp.login(os.environ["Server_ID"], os.environ["Server_PW"])
-            ftp.cwd('html/DATA')  # 업로드할 FTP 폴더로 이동
-            myfile = open(Data_Issues_Empty_File,'rb')  # 로컬 파일 열기
-            ftp.storbinary('STOR ' +Data_Issues_Empty_File, myfile )  # 파일을 FTP로 업로드
-            print(f"{now})  FTP {Data_Issues_Empty_File} 업로드 완료")
-            myfile.close()  # 파일 닫기
-            ftp.quit()
-            print(f"{now})  cc = 0 반환 완료")
-
-            config['Data'] = {}
-            config['Data']['Issues'] = issues
-            with open(Data_Issues_File, 'w', encoding='UTF-8-SIG') as configfile:
-                config.write(configfile)
-            
-            ftp = FTP('fxserver.dothome.co.kr')
-            ftp.login(os.environ["Server_ID"], os.environ["Server_PW"])
-            ftp.cwd('html/DATA')  # 업로드할 FTP 폴더로 이동
-            myfile = open(FileName_Issues,'rb')  # 로컬 파일 열기
-            ftp.storbinary('STOR ' +FileName_Issues, myfile )  # 파일을 FTP로 업로드
-            myfile.close()  # 파일 닫기
-            ftp.quit()
-            c = 1
-            
-
-    except UnboundLocalError: # 에러 종류
-        print(f"{now})  UnboundLocalError\n{now})    -특이 사항 또는 문제 없음")
-    except Exception as ex: # 에러 종류
-        print(f"{now})  Issues_Detected 에러 발생\n{now})    -{ex}")
       
 @client.event
 async def on_ready():
