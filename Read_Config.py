3a704057e0c424316b848dab3ef8a88cab1de2a6
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2021-11-23 01:15:41 +0900

    Update Read_Config.py

--- Read_Config.py
+++ Read_Config.py
@@ -2,12 +2,6 @@ import configparser
 
 Config = configparser.ConfigParser()
 
-def Config_Detect():
-    Config.read('Detect.ini', encoding='UTF-8-SIG') 
-    Config.sections()
-    Detect = int(Config['Data']['Detect'])
-    return Detect
-
 def Config_Title():
     Config.read('Data.ini', encoding='UTF-8-SIG') 
     Config.sections()
